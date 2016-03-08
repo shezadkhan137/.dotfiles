acbeb9fca5cbe9a122f0fb3f9dac9f37277050e4
Shezad Khan <shaz@revl.world>
2016-03-08 15:50:39 +0000

    Small fixes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,13 @@
-alias reload!='. ~/.zshrc'
-alias zshconfig="vim ~/.zshrc"
-alias ohmyzsh="vim ~/.oh-my-zsh"
-alias mkdirs="mkdir -p"
-alias vssh="vagrant ssh"
-alias vup="vagrant up"
-alias g="git"
-alias v="vagrant"
-alias rg="ranger"
-alias vi="vim"
-alias tmux="TERM=screen-256color-bce tmux"
+#alias reload!='. ~/.zshrc'
+#alias zshconfig="vim ~/.zshrc"
+#alias ohmyzsh="vim ~/.oh-my-zsh"
+#alias mkdirs="mkdir -p"
+#alias vssh="vagrant ssh"
+#alias vup="vagrant up"
+#alias g="git"
+#alias v="vagrant"
+#alias rg="ranger"
+#alias vi="vim"
+#alias tmux="TERM=screen-256color-bce tmux"
 
 
